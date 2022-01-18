739d0f9840c14a01daae9221c5e22fcfa45cc0cb
Anamaya1729 <73896949+Anamaya1729@users.noreply.github.com>
2022-01-18 13:58:56 +0530

    Create Question 2.cpp
    Given an array called A[] of sorted integers having no duplicates, find the length of the Longest Arithmetic Progression (LLAP) in it.

---
+++ Adobe/Question 2.cpp
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int lengthOfLongestAP(int set[], int n) {
+    if (n <= 2)  return n;
+    int L[n][n];
+    int llap = 2; 
+    for (int i = 0; i < n; i++)
+        L[i][n-1] = 2;
+
+    for (int j=n-2; j>=1; j--)
+    {
+        int i = j-1, k = j+1;
+        while (i >= 0 && k <= n-1)
+        {
+           if (set[i] + set[k] < 2*set[j])
+               k++;
+           else if (set[i] + set[k] > 2*set[j])
+           {   L[i][j] = 2, i--;   }
+
+           else
+           {
+
+               L[i][j] = L[j][k] + 1;
+               llap = max(llap, L[i][j]);
+               i--; k++;
+           }
+        }
+        while (i >= 0)
+        {
+            L[i][j] = 2;
+            i--;
+        }
+    }
+    return llap;
+
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int A[n];
+        for (int i = 0; i < n; i++) {
+            cin >> A[i];
+        }
+        Solution ob;
+        auto ans = ob.lengthOfLongestAP(A, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
