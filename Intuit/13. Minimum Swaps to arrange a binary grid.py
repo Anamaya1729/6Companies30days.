23073802dd3fba8b4560122e6baddd9bb5f209bd
Anamaya1729 <73896949+Anamaya1729@users.noreply.github.com>
2022-01-23 22:38:20 +0530

    Min no. of swaps to make grid valid
    Given an n x n binary grid, in one step you can choose two adjacent rows of the grid and swap them.
    
    A grid is said to be valid if all the cells above the main diagonal are zeros.
    
    Return the minimum number of steps needed to make the grid valid, or -1 if the grid cannot be valid.
    
    The main diagonal of a grid is the diagonal that starts at cell (1, 1) and ends at cell (n, n)

---
+++ Intuit/13. Minimum Swaps to arrange a binary grid.py
@@ -0,0 +1,27 @@
+class Solution:
+     def minSwaps(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        arr = []
+        S = set()
+        for row in grid:
+            c = 0
+            for i in row[::-1]:
+                if i:
+                    break
+                c += 1
+            while c in S:
+                c -= 1
+            if c >= 0:
+                S.add(c)
+                arr += c,
+            
+        if len(arr) < n:
+            return -1
+        
+        res = 0
+        for i, v in enumerate(arr):
+            for v2 in arr[i+1:]:
+                if v2 > v:
+                    res += 1
+        return res
+        
